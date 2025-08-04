a5439839c2e0e9ab685bce25d8bb3aee3a7e460b
w30010882 <wanglesong@huawei.com>
2025-08-04 20:37:34 +0800

    add script non reviewer alarm

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/coc.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/coc.py
@@ -5,6 +5,7 @@ import logging
 
 from huaweicloudsdksmn.v2 import PublishMessageRequest, PublishMessageRequestBody
 from c7n.utils import type_schema, local_session
+from c7n.filters import Filter
 from c7n.exceptions import PolicyValidationError
 from c7n_huaweicloud.provider import resources
 from c7n_huaweicloud.query import QueryResourceManager, TypeInfo
@@ -14,8 +15,8 @@ from huaweicloudsdkcore.exceptions import exceptions
 log = logging.getLogger("custodian.huaweicloud.resources.coc")
 
 
-@resources.register('coc')
-class Coc(QueryResourceManager):
+@resources.register('coc-patch')
+class CocPatch(QueryResourceManager):
     class resource_type(TypeInfo):
         service = 'coc'
         enum_spec = ('list_instance_compliant', 'instance_compliant', 'offset')
@@ -24,8 +25,8 @@ class Coc(QueryResourceManager):
         tag_resource_type = None
 
 
-@Coc.action_registry.register("non_compliant_alarm")
-class NonCompliantAlarm(HuaweiCloudBaseAction):
+@CocPatch.action_registry.register("patch_non_compliant_alarm")
+class PatchNonCompliantAlarm(HuaweiCloudBaseAction):
     """Alarm non compliant patch.
 
     :Example:
@@ -53,51 +54,176 @@ class NonCompliantAlarm(HuaweiCloudBaseAction):
                  value: 'cn-north-4'
                  op: eq
              actions:
-               - type: non_compliant_alarm
-                 smn: true
-                 region_id: cn-north-4
-                 topic_urn: ********
-                 subject: ********
-                 message: ********
+               - type: patch_non_compliant_alarm
+                 topic_urn_list:
+                   - "urn:smn:region:account-id:topic-name"
+                 subject: ""
+                 message: ""
     """
 
-    schema = type_schema("non_compliant_alarm",
-                         smn={'type': 'boolean'},
-                         region_id={'type': 'string'},
-                         topic_urn={'type': 'string'},
+    schema = type_schema("patch_non_compliant_alarm",
+                         required=['topic_urn_list'],
+                         topic_urn_list={'type': 'array', 'items': {'type': 'string'}},
                          subject={'type': 'string'},
                          message={'type': 'string'}
                          )
 
-    def validate(self):
-        smn = self.data.get('smn', False)
-        if smn and not (self.data.get('region_id') and self.data.get('topic_urn') and
-                        self.data.get('subject') and self.data.get('message')):
-            raise PolicyValidationError("Can not create smn alarm message when parameter is error.")
+    def process(self, resources):
+        """Process message sending logic"""
+        if not resources:
+            return resources
 
-    def perform_action(self, resource):
-        if not self.data.get('smn', False):
-            log.info("Do not create smn alarm message.")
-            return
-        ecs_name = resource.get('name')
-        region = resource.get('region')
-        ecs_instance_id = resource.get('instance_id')
-        non_compliant_count = resource.get('non_compliant_summary').get('non_compliant_count')
-        message_data = (f'ecs_name: {ecs_name}, ecs_instance_id: {ecs_instance_id}, '
-                        f'region: {region}, non_compliant_count: {non_compliant_count}')
-        subject = self.data.get('subject')
-        message = self.data.get('message')
-        topic_urn = self.data.get('topic_urn')
+        topic_urn_list = self.data.get('topic_urn_list', None)
+        subject = self.data.get('subject', 'Patch Security Non-Compliance Alert')
+        message = self.data.get('message', 'Incompliant patches are detected on the following hosts, '
+                                           'Repair the patches in time.')
 
-        client = local_session(self.manager.session_factory).client('smn')
-        message_body = PublishMessageRequestBody(
-            subject=subject,
-            message=message + '\n' + message_data
-        )
-        request = PublishMessageRequest(topic_urn=topic_urn, body=message_body)
-        try:
-            response = client.publish_message(request)
-            log.info(f"Successfully create smn alarm message, the message id: "
-                     f"{response.message_id}.")
-        except exceptions.ClientRequestException as e:
-            log.error(f"Create smn alarm message failed: {e.error_msg}")
+        if not topic_urn_list and len(topic_urn_list) < 1:
+            raise PolicyValidationError("Can not create smn alarm message when parameter:topic_urn is None.")
+
+        patch_non_compliant_data = ''
+        for resource in resources:
+            ecs_name = resource.get('name')
+            region = resource.get('region')
+            ecs_instance_id = resource.get('instance_id')
+            non_compliant_count = resource.get('non_compliant_summary').get('non_compliant_count')
+            patch_non_compliant_data = (f'ecs_name: {ecs_name}, ecs_instance_id: {ecs_instance_id}, '
+                                        f'region: {region}, non_compliant_count: {non_compliant_count}')
+
+        for topic_urn in topic_urn_list:
+            client = local_session(self.manager.session_factory).client('smn')
+            message_body = PublishMessageRequestBody(
+                subject=subject,
+                message=message + '\n' + patch_non_compliant_data
+            )
+            request = PublishMessageRequest(topic_urn=topic_urn, body=message_body)
+            try:
+                response = client.publish_message(request)
+                log.info("[actions]-[patch_non_compliant_alarm]-The resource:[coc-patch] "
+                         f"send message to {topic_urn} has succeeded, the smn message id: {response.message_id}.")
+            except exceptions.ClientRequestException as e:
+                log.error("[actions]-[patch_non_compliant_alarm]-The resource:[coc-patch] "
+                          f"send message to {topic_urn} failed: {e.error_msg}")
+                raise e
+
+
+@resources.register('coc-script')
+class CocScript(QueryResourceManager):
+    class resource_type(TypeInfo):
+        service = 'coc'
+        enum_spec = ('list_scripts', 'data', 'marker')
+        id = 'id'
+        offset_start_num = 1
+        tag_resource_type = None
+
+
+@CocScript.filter_registry.register("script_non_reviewer")
+class ScriptNonReviewerFilter(Filter):
+    """Check if a script does not have an assigned reviewer.
+
+    .. code-block:: yaml
+
+      policies:
+       - name: script_non_reviewer
+         resource: huaweicloud.coc-script
+         filters:
+           - script_non_reviewer_filter
+    """
+    schema = type_schema('script_non_reviewer_filter')
+
+    def process(self, resources):
+        """Filter scripts without configured reviewers
+        """
+        if not resources:
+            return resources
+        filter_resources = []
+        for resource in resources:
+            script_list = resource.get('data', [])
+            if len(script_list) == 0:
+                continue
+            for script in script_list:
+                properties = script.get('properties', None)
+                if not properties:
+                    continue
+                reviewers = properties.get('reviewers', [])
+                if len(reviewers) == 0:
+                    filter_resources.append(script)
+        return filter_resources
+
+
+@CocScript.action_registry.register("script_non_reviewer_alarm")
+class ScriptNonReviewerAlarm(HuaweiCloudBaseAction):
+    """Alarm non reviewer script.
+
+    Used to send notification messages regarding scripts without configured reviewers,
+    including script name and script ID information.
+
+    :Example:
+
+    .. code-block:: yaml
+
+         policies:
+           - name: script_non_reviewer_alarm
+             resource: huaweicloud.coc
+             filters:
+               - type: value
+                 key: risk_level
+                 op: in
+                 value:
+                   - xxxx
+                   - xxxx
+             actions:
+               - type: script_non_reviewer_alarm
+                 topic_urn_list:
+                   - "urn:smn:region:account-id:topic-name"
+                 subject: ""
+                 message: ""
+    """
+
+    schema = type_schema("script_non_reviewer_alarm",
+                         required=['topic_urn_list'],
+                         topic_urn_list={'type': 'array', 'items': {'type': 'string'}},
+                         subject={'type': 'string'},
+                         message={'type': 'string'}
+                         )
+
+    def process(self, resources):
+        """Process message sending logic"""
+        if not resources:
+            return resources
+
+        topic_urn_list = self.data.get('topic_urn_list', None)
+        subject = self.data.get('subject', 'Script Security Non-Compliance Alert')
+        message = self.data.get('message', 'The following script has been found to lack an assigned reviewer, '
+                                           'which constitutes a security non-compliance. '
+                                           'Please promptly assign a reviewer.')
+
+        if not topic_urn_list and len(topic_urn_list) < 1:
+            raise PolicyValidationError("Can not create smn alarm message when parameter:topic_urn is None.")
+
+        script_non_reviewer_data = ''
+        for resource in resources:
+            script_id = resource.get('script_uuid')
+            script_name = resource.get('name', '')
+            operator = resource.get('operator', '')
+            properties = resource.get('properties', '')
+            risk_level = properties.get('risk_level') if not properties else ''
+
+            script_non_reviewer_data += (f'script_id: {script_id}, script_name: {script_name}, operator: {operator}, '
+                                         f'risk_level: {risk_level}\n')
+
+        for topic_urn in topic_urn_list:
+            client = local_session(self.manager.session_factory).client('smn')
+            message_body = PublishMessageRequestBody(
+                subject=subject,
+                message=message + '\n' + script_non_reviewer_data
+            )
+            request = PublishMessageRequest(topic_urn=topic_urn, body=message_body)
+            try:
+                response = client.publish_message(request)
+                log.info("[actions]-[script_non_reviewer_alarm]-The resource:[coc-script] "
+                         f"send message to {topic_urn} has succeeded, the smn message id: {response.message_id}.")
+            except exceptions.ClientRequestException as e:
+                log.error("[actions]-[script_non_reviewer_alarm]-The resource:[coc-script] "
+                          f"send message to {topic_urn} failed: {e.error_msg}")
+                raise e
---
+++ tools/c7n_huaweicloud/tests/test_coc.py
@@ -0,0 +1,72 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+from huaweicloud_common import BaseTest
+
+
+class CocAlarmTest(BaseTest):
+
+    def test_patch_non_compliant_query(self):
+        factory = self.replay_flight_data('patch_non_compliant_alarm')
+        p = self.load_policy({
+            'name': 'list_instance_compliant',
+            'resource': 'huaweicloud.coc-patch'},
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]['status'], "non_compliant")
+
+    def test_patch_non_compliant_alarm(self):
+        factory = self.replay_flight_data('patch_non_compliant_alarm')
+        p = self.load_policy({
+            'name': 'list_instance_compliant',
+            'resource': 'huaweicloud.coc-patch',
+            'filters': [
+                {
+                    'type': 'value',
+                    'key': 'status',
+                    'value': 'non_compliant'
+                },
+                {
+                    'type': 'value',
+                    'key': 'report_scene',
+                    'value': 'ECS'
+                }
+            ],
+            "actions": [{
+                "type": "patch_non_compliant_alarm",
+                "topic_urn_list": ["urn:smn:region:account-id:topic-name"],
+                "subject": "Machine Non-compliant Patch Version Alert",
+                "message": "There are machines with non compliant patch versions installed under your account"
+            }]
+        },
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]['status'], "non_compliant")
+
+    def test_script_non_reviewer_query(self):
+        factory = self.replay_flight_data('script_non_reviewer_alarm')
+        p = self.load_policy({
+            'name': 'list_scripts',
+            'resource': 'huaweicloud.coc-script'},
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources['data'][0]["total"], 212)
+
+    def test_script_non_reviewer_query(self):
+        factory = self.replay_flight_data('script_non_reviewer_alarm')
+        p = self.load_policy({
+            "name": "script_non_reviewer_alarm",
+            "resource": "huaweicloud.coc-script",
+            "actions": [{
+                "type": "script_non_reviewer_alarm",
+                "topic_urn_list": ["urn:smn:region:account-id:topic-name"],
+                "subject": "Script Security Non-Compliance Alert",
+                "message": "The following script has been found to lack an assigned reviewer"
+            }]
+        },
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]["name"], "gzz_213546")
